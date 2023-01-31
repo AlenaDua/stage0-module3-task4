5e2c855f1ac2d177b11e0d841366417cb81ee201
Alena Dubadzelava <dubodelova.minsk@gmail.com>
2023-01-31 21:32:21 +0100

    initial commit

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,18 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int intC = 99;
+        char charC = (char) intC;
+
+        int intH = 104;
+        char charH = (char) intH;
+
+        int intA = 97;
+        char charA = (char) intA;
+
+        int intR = 114;
+        char charR = (char) intR;
+
+        System.out.printf("%c, %c, %c, %c", charC, charH, charA, charR);
     }
 }
